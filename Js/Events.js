93dff87b684410f9a2ad2c73d1763a9d88ef6d8b
waqaramjad <waqaramjad345@gmail.com>
2017-09-13 11:10:51 +0500

    Add Event with its Js file

---
+++ Js/Events.js
@@ -0,0 +1,21 @@
+var EventName = document.getElementById('EventName');
+var hostName = document.getElementById('hostName');
+var  Location= document.getElementById('Location');
+var Date1 = document.getElementById('Date');
+var Time = document.getElementById('Time');
+
+function addEvent()
+{
+    var event =
+    {
+        EventName:EventName.value,
+        hostName:hostName.value,
+        Location:location.value,
+        Date:Date1.value,
+        Time:Time.value
+
+
+
+    }
+
+}
---
+++ Js/firebase.js
@@ -0,0 +1,12 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBq6Hh5NTx-o6zzOvb8HKXClECobZ6FXVQ",
+    authDomain: "event-planner-waqar.firebaseapp.com",
+    databaseURL: "https://event-planner-waqar.firebaseio.com",
+    projectId: "event-planner-waqar",
+    storageBucket: "event-planner-waqar.appspot.com",
+    messagingSenderId: "578222618804"
+  };
+  firebase.initializeApp(config);
